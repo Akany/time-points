761a82f9d4628af5ea3678b82077e49c3d9d12db
Olya Markysh <december.olka51@gmail.com>
2020-08-19 14:14:32 +0300

    main page view, store, routes

--- packages/client/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import {App} from './app.jsx';
-
-const holder = document.createElement('div');
-
-ReactDom.render(<App />, holder, () => document.body.appendChild(holder));
